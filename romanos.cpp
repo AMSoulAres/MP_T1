51822b4d20ccb7947d52ac36478e1dc2adcff88c
Gabriel Amorim Soares e Silva <gabrielgabel03@gmail.com>
2023-04-26 21:54:28 -0300

    adicionando conversor

--- romanos.cpp
+++ romanos.cpp
@@ -1,5 +1,34 @@
 #include "romanos.hpp"
-int romanos_para_decimal(char const * num_romano)
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int romanos_para_decimal(char const *num_romano)
 {
-  return 0; 
+    map<char, int> associaNumero = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}};
+
+        std::string romano = num_romano;
+
+    int resultado = 0, caractereAnterior = 0, caractereAtual = 0;
+
+    for (int i = romano.length() - 1; i >= 0; i--){
+        caractereAtual = associaNumero[romano[i]];
+
+        if (caractereAtual >= caractereAnterior){
+            resultado += caractereAtual;
+        } else {
+            resultado -= caractereAtual;
+        }
+        caractereAnterior = caractereAtual;
+        
+        return resultado;
+    }
 }
